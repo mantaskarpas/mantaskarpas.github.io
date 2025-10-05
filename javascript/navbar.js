16ec770f0397b7b19af5701af2328185b6b7e395
Mantas <mantas.karpas@outlook.com>
2025-10-05 15:11:55 +0100

    Add files via upload

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -4,3 +4,12 @@ const navLinks = document.getElementById('nav-links');
 toggle.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
+
+document.addEventListener("DOMContentLoaded", function (){
+    const toggle = document.getElementById('menu-toggle');
+    const navLinks = document.getElementById('nav-links');
+
+    toggle.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+});
--- javascript/repairs.js
+++ javascript/repairs.js
@@ -1,6 +1,4 @@
 function bookRepair() {
   alert("Redirecting to the booking page...");
   // window.location.href = "booking.html"; // Uncomment when ready
-
-
 }
