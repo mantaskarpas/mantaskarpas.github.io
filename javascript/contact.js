3fa78b955f6e1b1e5bdef96ee3dce9909d17d58a
Mantas <mantas.karpas@outlook.com>
2025-10-13 20:44:44 +0100

    Add files via upload

--- javascript/contact.js
+++ javascript/contact.js
@@ -16,3 +16,11 @@ function validateForm() {
     alert("Message sent successfully!");
     return true;
 }
+
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
